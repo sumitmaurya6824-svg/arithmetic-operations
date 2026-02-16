a420372b9cc2885b76ed7b9b242419d4d930a806
sumitmaurya6824-svg <sumitmaurya6824@gmail.com>
2026-02-16 14:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,12 @@ function calculate() {
   let result;
 
   switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-
-    case '-':
-      result = num1 - num2;
-      break;
-
-    case '*':
-      result = num1 * num2;
-      break;
-
-    case '/':
-      result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
-      break;
-
-    case '%':
-      result = num1 % num2;
-      break;
-
-    default:
-      result = "Invalid operation";
+    case '+': result = num1 + num2; break;
+    case '-': result = num1 - num2; break;
+    case '*': result = num1 * num2; break;
+    case '/': result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero"; break;
+    case '%': result = num1 % num2; break;
+    default: result = "Invalid";
   }
 
   document.getElementById("result").innerText = "Result: " + result;
