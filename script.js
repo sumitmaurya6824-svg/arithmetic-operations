25b472f4f3a00899d3101941cd23672a3496b328
sumitmaurya6824-svg <sumitmaurya6824@gmail.com>
2026-02-16 14:19:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function calculate() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let operator = document.getElementById("operator").value;
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+
+    case '*':
+      result = num1 * num2;
+      break;
+
+    case '/':
+      result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+      break;
+
+    case '%':
+      result = num1 % num2;
+      break;
+
+    default:
+      result = "Invalid operation";
+  }
+
+  document.getElementById("result").innerText = "Result: " + result;
+}
